f3e6b4e121b64f006a6efd6fca90bb3e3bb1f9fe
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2024-08-07 09:33:39 +0100

    part2

--- src/main/scala/Day18.scala
+++ src/main/scala/Day18.scala
@@ -24,12 +24,8 @@ object  Day18 :
         val width = input.head.length - 1
         val height = input.length - 1
         val newInput = input.zipWithIndex.map((l,i) => {l.zipWithIndex.map( (c,j) => {
-            (j, i) match
-                case (0,0) => '#'
-                case (0, height) => '#'
-                case (width, 0) => '#'
-                case (width, height) => '#'
-                case _ => input(i)(j)
+            if((i==0 && j==0) || (i==0 && j==width) || (i==height && j==0) || (i==height && j==width)) '#'
+            else input(i)(j)
         }).mkString })
         if (steps < 1) newInput.map(_.count(c => c == '#')).sum
         else {
@@ -38,7 +34,7 @@ object  Day18 :
                     toLive(newInput, Position(j, i))
                 }).mkString
             })
-            part1(newBoard, steps - 1)
+            part2(newBoard, steps - 1)
         }
     }
 
@@ -53,21 +49,7 @@ object  Day18 :
         if(countAlive == 3 || (countAlive == 2 && input(pos.y)(pos.x) == '#')) '#'
         else '.'
     }
-
-    private def toLivePart2(input: Seq[String], pos: Position): Character = {
-        val countAlive = pos.neighbours.map(p => {
-            if (p.x < 0 || p.x >= input.head.length || p.y < 0 || p.y >= input.length)
-                '.'
-            else
-                input(p.y)(p.x)
-        }).count(_ == '#')
-
-        if (countAlive == 3 || (countAlive == 2 && input(pos.y)(pos.x) == '#')) {'#'}
-        else if ((pos.x == 0 && pos.y == 0) || (pos.x == 0 && pos.y == input.length -1) || (pos.x == input.head.length -1 && pos.y == 0) || (pos.x == input.head.length -1 && pos.y == input.length - 1)) {'#'}
-        else '.'
-    }
-
-
+    
     def main(args: Array[String]): Unit = {
         val data = io.Source.fromResource("Day18.txt").getLines().toSeq
         println(part1(data, 100))
